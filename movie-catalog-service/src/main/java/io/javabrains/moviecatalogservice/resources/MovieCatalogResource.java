0a157798fe006c26b3b1554b872bf47c85a163c6
Jessica Rodrigues-Dlouhy <jessiro2@cisco.com>
2023-02-16 11:07:28 -0500

    fixing issue with pom.xml - version of eureka client

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -34,12 +34,12 @@ public class MovieCatalogResource {
         RestTemplate restTemplate = new RestTemplate();
 
         //Hard-coded - get all rated movie IDs
-        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
+        UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/" + userId, UserRating.class);
 
 
         return ratings.getUserRating().stream().map(rating ->  {
             // For each movie ID, call movie info service and get details
-           Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+           Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
 
            //Put them all together
            return new CatalogItem(movie.getName(), "Description", rating.getRating());
